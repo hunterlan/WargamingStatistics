21b83ec1aa3fe4d5a906e6731e27d73590f4efec
hunterlan <kotyshe@gmail.com>
2019-11-11 21:59:44 +0200

    Added new field

--- WotStatistics/WargamingAPI/Model/Statistics.cs
+++ WotStatistics/WargamingAPI/Model/Statistics.cs
@@ -20,6 +20,8 @@ namespace WotStatistics.Model
 
         public int Rating { get; set; }
 
+        public int WN8 { get; set; }
+
         public override string ToString()
         {
             string statistic = "Последний бой был " + LastBattle.ToString() +
